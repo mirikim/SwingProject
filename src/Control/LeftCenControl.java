f95244d13cb3a94d21f10270938cc2cd20261aaf
bingoori <woosungdoo@gmail.com>
2016-01-26 14:36:38 +0900

    ㅇㅇㅇ아직테스트

---
+++ src/Control/LeftCenControl.java
@@ -0,0 +1,25 @@
+package Control;
+
+import javax.swing.JOptionPane;
+
+import View.LeftPan;
+
+public class LeftCenControl {
+	LeftPan lp = new LeftPan();
+	String seatLocation = ""; // 좌석위치
+	String nt = ""; // 입실시간
+	String et = ""; // 퇴실
+
+	public void seatClick() {
+		System.out.println("호출됫냐");
+		//lp.LeftPanClear();
+	}
+
+	public void getshowMessage(String seatlocation, String nt, String et) {
+		this.seatLocation = seatlocation;
+		this.nt = nt;
+		this.et = et;
+		JOptionPane.showMessageDialog(null, "좌석 : " + seatLocation + "\n입실 시간 : " + nt + "\n퇴실 : " + et);
+	}
+
+}
